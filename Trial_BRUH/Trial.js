03eb7697abc1dc7843764ce25d5546b7e7b3973e
Tito Ghofara <111202012775@mhs.dinus.ac.id>
2022-01-20 21:37:31 +0700

    Tugas Akhir

---
+++ Trial_BRUH/Trial.js
@@ -0,0 +1,6 @@
+var yakin = confirm("Apakah kamu yakin akan mengunjungi website UDINUS ;)");
+if (yakin){
+    window.location = "http://dinus.ac.id";
+}else{
+    alert ("Belum , tetap disini saja");
+}
